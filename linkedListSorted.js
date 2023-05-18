d24721cca2b6cd5dcd4a08caa9a65db797445237
HOVO777 <111977958+HovoBoryan@users.noreply.github.com>
2023-05-18 19:11:14 +0400

    Update linkedListSorted.js

--- linkedListSorted.js
+++ linkedListSorted.js
@@ -12,19 +12,19 @@ class LinkedList {
         }
 
     isEmpty() {
-        //Checks if the list is empty
+            //Checks if the list is empty
         return this.head === null;
         }
 
     append(data) {
         const newNode = new Node(data);
-        // If the list is empty the new element becomes the head
+            // If the list is empty the new element becomes the head
         if (!this.head) {
             this.head = newNode;
                 return;
         }
 
-        //If new element less then head of list, it becomes the new head 
+            //If new element less then head of list, it becomes the new head 
         if (data < this.head.data) {
             newNode.next = this.head;
             this.head = newNode;
@@ -32,16 +32,16 @@ class LinkedList {
         }
         let current = this.head;
 
-        // Find the position to insert the new element
-        while (current.next && data > current.next.data) {
+            // Find the position to insert the new element
+        while ((current.next && data) > current.next.data) {
         current = current.next;
         }
-        //Insert a new element after the current element
+            //Insert a new element after the current element
         newNode.next = current.next;
         current.next = newNode;
     }
 
-    // Method to get the largest element of a list
+            // Method to get the largest element of a list
     getMax() {
         if (!this.head) {
             // return null when list is empty
@@ -58,7 +58,7 @@ class LinkedList {
                 return max;
     }
 
-        // Method to get the smallest element of a list
+            // Method to get the smallest element of a list
     getMin() {
         if(!this.head) {
             // return null when list is empty
@@ -75,28 +75,28 @@ class LinkedList {
                 return min;
     }
 
-        //function that deletes an element from a list 
+            //function that deletes an element from a list 
     delete(data) { 
         if (!this.head) {
                 return;
         }
-        // if deleted element is the head of the list
+            // if deleted element is the head of the list
         if (this.head.data === data) {
             this.head = this.head.next;
                 return;
         }
         let current = this.head;
-        // Look for the element to be removed and save the previous element
+            // Look for the element to be removed and save the previous element
         while (current.next && current.next.data !== data) {
                 current = current.next;
         }
-        // If the removed element is found, reconnect the links
+            // If the removed element is found, reconnect the links
         if (current.next && current.next.data === data) {
             current.next = current.next.next;
         }
     }
         
-    //Presents the list as an array
+            //Presents the list as an array
     toArray() {
         const array = [];
         let current = this.head;
@@ -120,8 +120,8 @@ class LinkedList {
 
   console.log(list.toArray()); // Output:   [ 11, 11, 22, 33, 44, 777 ]
 
-    list.delete(22); // deletes 22 from the list
-    list.append(111); // I add an element again
+    list.delete(22);           // deletes 22 from the list
+    list.append(111);          // I add an element again
 
   console.log(list.toArray()); // Output:  [ 11 11, 33, 44, 111, 777 ]
   console.log(`${list.getMax()}  is the largest element of your list`); // Output:  777
